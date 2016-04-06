57bc0a903f8a7ef673395b3af6cb3273749ce055
thegauraw <thegauraw@gmail.com>
2016-04-06 12:47:57 +1000

    Add constraints for move method.

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,7 +1,7 @@
 class Robot
   attr_accessor :x, :y, :f
   DIRECTION = ['NORTH', 'EAST', 'SOUTH', 'WEST']
-  # SIZE = 5
+  TABLE_SIZE = 5
 
   def place(x,y,f)
     @x = x
@@ -23,4 +23,15 @@ class Robot
     @f = DIRECTION[(current_index + 1)%4]
   end
 
+  def move
+    if @f == 'NORTH'
+      @y += 1 if @y < TABLE_SIZE-1
+    elsif @f == 'SOUTH'
+      @y -= 1 if @y > 0
+    elsif @f == 'EAST'
+      @x += 1 if @x < TABLE_SIZE-1
+    elsif @f == 'WEST'
+      @x -= 1 if @x > 0
+    end
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -72,4 +72,47 @@ describe Robot do
 
   end
 
+  describe '#move' do
+    context 'is at the end/edge in that direction' do
+      context 'has y-axis 0 and is facing SOUTH' do
+
+        it 'does not change the state/position' do
+          robot.place(0,0,'SOUTH')
+          robot.move
+          expect(robot.report).to eql "Output: 0,0,SOUTH"
+        end
+      end
+      context 'has x-axis 0 and is facing WEST' do
+
+        it 'does not change the state/position' do
+          robot.place(0,0,'WEST')
+          robot.move
+          expect(robot.report).to eql "Output: 0,0,WEST"
+        end
+      end
+
+      context 'has y-axis TABLE_SIZE and is facing NORTH' do
+
+        it 'does not change the state/position' do
+          robot.place(Robot::TABLE_SIZE,Robot::TABLE_SIZE,'NORTH')
+          robot.move
+          expect(robot.report).to eql "Output: #{Robot::TABLE_SIZE},#{Robot::TABLE_SIZE},NORTH"
+        end
+      end
+      context 'has x-axis TABLE_SIZE and is facing EAST' do
+
+        it 'does not change the state/position' do
+          robot.place(Robot::TABLE_SIZE,Robot::TABLE_SIZE,'EAST')
+          robot.move
+          expect(robot.report).to eql "Output: #{Robot::TABLE_SIZE},#{Robot::TABLE_SIZE},EAST"
+        end
+      end
+    end
+
+    # context 'is at location other than edge in that direction' do
+    #   context 'is facing ' do
+    #     it 'change x-coordinate'
+    #   end
+    # end
+  end
 end
